6d03eb60c3ef58062798aace6c4a18b7f15695b3
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-30 14:56:51 -0400

    1-5

---
+++ demos/1-5-javascript-objects.js
@@ -0,0 +1,18 @@
+// let employeeId = "1";
+// let name = "Ezra";
+// let jobTitle = "Theatre Teacher";
+// let payRate = 38.46;
+
+// function doSomethingWithEmployee(employeeId, name, jobTitle, payRate) {}
+
+// doSomethingWithEmployee(employeeId, name, jobTitle, payRate);
+
+let object = {};
+let array = [];
+
+let employee = {
+  employeeId: "1",
+  name: "Ezra",
+  jobTitle: "Theatre Teacher",
+  payRate: 38.46,
+};
