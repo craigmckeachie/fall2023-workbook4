195f24327d713ea37c85ce308e3055f85b377d78
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-30 15:58:00 -0400

    solution to product code

---
+++ exercises/ObjectScripts/product_code.js
@@ -0,0 +1,30 @@
+let partCode1 = "XYZ:1234-L";
+
+function parsePartCode(partCode) {
+  return {
+    supplierCode: getSupplierCode(partCode),
+    productNumber: getProductNumber(partCode),
+    size: getSize(partCode),
+  };
+}
+
+function getSupplierCode(partCode) {
+  const positionOfColon = partCode.indexOf(":");
+  return partCode.substring(0, positionOfColon);
+}
+
+function getProductNumber(partCode) {
+  const positionOfColon = partCode.indexOf(":");
+  const positionOfHyphen = partCode.indexOf("-");
+  return partCode.substring(positionOfColon + 1, positionOfHyphen);
+}
+
+function getSize(partCode) {
+  const positionOfHyphen = partCode.indexOf("-");
+  return partCode.substring(positionOfHyphen + 1);
+}
+
+let part1 = parsePartCode(partCode1);
+console.log("Supplier: " + part1.supplierCode + 
+            " Product Number: " + part1.productNumber + 
+            " Size: " + part1.size);
