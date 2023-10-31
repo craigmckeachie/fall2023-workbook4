56ab4b165206613980bc72743b811a26a93195d8
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-31 10:42:28 -0400

    update

---
+++ demos/2-4-while-loops.js
@@ -0,0 +1,6 @@
+
+let count = 1;
+while (count<=100) {
+    console.log("did something difficult");
+    count = count + 1;
+}
--- exercises/ObjectScripts/product_code.js
+++ exercises/ObjectScripts/product_code.js
@@ -1,3 +1,4 @@
+//Dave
 function parsePartCode(partCode) {
   return {
     supplierCode: getSupplierCode(partCode),
@@ -6,23 +7,27 @@ function parsePartCode(partCode) {
   };
 }
 
+//Kevin
 function getSupplierCode(partCode) {
   const positionOfColon = partCode.indexOf(":");
   return partCode.substring(0, positionOfColon);
 }
 
+//Bob
 function getProductNumber(partCode) {
   const positionOfColon = partCode.indexOf(":");
   const positionOfHyphen = partCode.indexOf("-");
   return partCode.substring(positionOfColon + 1, positionOfHyphen);
 }
 
+//Phil
 function getSize(partCode) {
   const positionOfHyphen = partCode.indexOf("-");
   return partCode.substring(positionOfHyphen + 1);
 }
 
 let partCode1 = "XYZ:1234-L";
+//Gru
 let part1 = parsePartCode(partCode1);
 console.log(part1);
 console.log("Supplier: " + part1.supplierCode + " Product Number: " + part1.productNumber + " Size: " + part1.size);
