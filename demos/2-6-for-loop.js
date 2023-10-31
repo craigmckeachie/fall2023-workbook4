395d0f1ddfe1038d32a4036205fef0abf23065b5
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-31 11:21:25 -0400

    for loop

---
+++ demos/2-6-for-loop.js
@@ -0,0 +1,17 @@
+//part 1: code that executes before the loop begins
+let count = 1;
+
+// part 2: condition that must be true for the loop to keep executing
+while (count <= 10) {
+  console.log("did something difficult | while loop");
+  //part 3: code that runs at the bottom of each iteration
+  count = count + 1;
+}
+
+// for (part1; part2; part3) {
+
+// }
+
+for (let count = 1; count <= 10; count = count + 1) {
+    console.log("did something difficult | for loop");
+}
