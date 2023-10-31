38a882430b56f3f69d21b4f50e19a61d4e3b3929
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-31 10:48:20 -0400

    add comments

--- demos/2-4-while-loops.js
+++ demos/2-4-while-loops.js
@@ -1,6 +1,10 @@
+//simple while loop
 
+//this variable keeps track the number of times the loop is run
 let count = 1;
-while (count<=100) {
-    console.log("did something difficult");
-    count = count + 1;
+
+while (count <= 100) {
+  //stuff in here is going to happen 100 times  
+  console.log("did something difficult");
+  count = count + 1; //incrementing the loop so it doesn't run infinitely
 }
