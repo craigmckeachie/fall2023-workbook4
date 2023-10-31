a6ab4fa36431bba983e4e0b1fea7907023637087
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-31 14:05:52 -0400

    comments to for example

--- demos/tv-shows-array.js
+++ demos/tv-shows-array.js
@@ -1,19 +1,19 @@
-let shows = ["Breaking Bad","Better Call Saul", "Friends", "Supernatural", "Brooklyn 99", "Modern Family"]
+let shows = ["Breaking Bad", "Better Call Saul", "Friends", "Supernatural", "Brooklyn 99", "Modern Family"];
+//for loop, need to understand but not memorize
+for (let count = 0; count < shows.length; count++) {
+  console.log(shows[count]);
+}
 
+//for loop using index as the name of the count variable
 for (let index = 0; index < shows.length; index++) {
-    const show = shows[index];
-    console.log(show);  
+  const show = shows[index];
+  console.log(show);
 }
 
+//for of loop, practical, will be used day to day
 for (const show of shows) {
-    console.log(show);
+  console.log(show);
 }
-
-
-// for(let count =0; count< shows.length; count++){
-//     console.log(shows[count]);
-// }
-
 
 // console.log(shows[0]);
 // console.log(shows[1]);
