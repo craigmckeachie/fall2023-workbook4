0636683f0592c373c05b931d78b4e319cdf86352
Craig McKeachie <craigdmckeachie@gmail.com>
2023-10-31 13:33:25 -0400

    tv shows array

--- demos/tv-shows-array.js
+++ demos/tv-shows-array.js
@@ -1,6 +1,12 @@
 let shows = ["Breaking Bad","Better Call Saul", "Friends", "Supernatural"]
-console.log(shows[0]);
-console.log(shows[1]);
-console.log(shows[2]);
-console.log(shows[3]);
-console.log(shows[4]);
+
+for(let count =0; count<4; count++  ){
+    console.log(shows[count]);
+}
+
+
+// console.log(shows[0]);
+// console.log(shows[1]);
+// console.log(shows[2]);
+// console.log(shows[3]);
+// console.log(shows[4]);
