94edc1ba6272190055350e966157e938af9c77f1
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-01 15:31:48 -0400

    actors 1

---
+++ exercises/actors.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let academyMembers = [
+  {
+    memID: 101,
+    name: "Bob Brown",
+    films: ["Bob I", "Bob II", "Bob III", "Bob IV"],
+  },
+  {
+    memID: 142,
+    name: "Sallie Smith",
+    films: ["A Good Day", "A Better Day"],
+  },
+  {
+    memID: 187,
+    name: "Fred Flanders",
+    films: ["Who is Fred?", "Where is Fred?", "What is Fred?", "Why Fred?"],
+  },
+  {
+    memID: 203,
+    name: "Bobbie Boots",
+    films: ["Walking Boots", "Hiking Boots", "Cowboy Boots"],
+  },
+];
