4653e0e725944d2642e9cae04059f69b11e31512
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-01 09:19:52 -0400

    avg scores complete

--- exercises/avg_scores.js
+++ exercises/avg_scores.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let myScores = [92, 98, 84, 76, 89, 99, 100];
-let yourScores = [82, 98, 94, 88, 92, 100, 100];
+let yourScores = [82, 98, 94, 88, 92];
 
 function getAverage(scores) {
   let total = 0;
