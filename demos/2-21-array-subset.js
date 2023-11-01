285fcd25f2380e5e9e2f50ecfebb0707e0b46b96
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-01 10:24:58 -0400

    2-21

--- demos/2-21-array-subset.js
+++ demos/2-21-array-subset.js
@@ -1,22 +1,32 @@
-let menu = [
-  { id: 1, item: "Tacos", category: "Meal", price: 12.29 },
-  { id: 2, item: "Burger", category: "Meal", price: 7.29 },
-  { id: 3, item: "Salad", category: "Meal", price: 8.29 },
+let menuItems = [
+  { id: 1, item: "Tacos", category: "Meal", price: 12.29 }, //menuItem
+  { id: 2, item: "Burger", category: "Meal", price: 7.29 }, //menuItem
+  { id: 3, item: "Salad", category: "Meal", price: 8.29 }, //menuItem
   { id: 4, item: "Ice tea", category: "Drink", price: 2.19 },
   { id: 5, item: "Coke", category: "Drink", price: 2.29 },
 ];
 
-function getMenuItemsInCategory(menu, category) {
+let menuItems2 = ["Tacos", "Burgers", "Salad"]
+
+function getMenuItemsInCategory(menuItems, category) {
   let matchingMenuItems = [];
  
-  for (const menuItem of menu) {
+  for (const menuItem of menuItems) {
     if (menuItem.category == category) {
         matchingMenuItems.push(menuItem);
     }
   }  
 
+  for (let index = 0; index < menuItems.length; index++) {
+    const menuItem = menuItems[index];
+    console.log(menuItem);
+  }
+
   return matchingMenuItems;
 }
 
-let drinks = getMenuItemsInCategory(menu, "Drink");
+let drinks = getMenuItemsInCategory(menuItems, "Drink");
 console.log(drinks);
+// let meals = getMenuItemsInCategory(menuItems, "Meal");
+// console.log(meals);
+
