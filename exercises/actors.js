14aa29177b91aaaeb3f804dc137ab5dc4d9a2563
Craig McKeachie <craigdmckeachie@gmail.com>
2023-11-01 16:01:23 -0400

    actors

--- exercises/actors.js
+++ exercises/actors.js
@@ -1,7 +1,6 @@
 "use strict";
 
-
-// array (members) -> object (member) -> array (films | member.films) 
+// array (members) -> object (member) -> array (films | member.films)
 let academyMembers = [
   {
     memID: 101,
@@ -25,14 +24,36 @@ let academyMembers = [
   },
 ];
 
-// Who is the Academy Member whose ID is 187?
-
+console.log("Who is the Academy Member whose ID is 187?");
 for (const member of academyMembers) {
+  if (member.memID == 187) {
     console.log(member.name);
+  }
 }
 
+console.log("Who has have been in at least 3 films?");
+for (const member of academyMembers) {
+  if (member.films.length >= 3) {
+    console.log(member.name);
+  }
+}
 
-// Who has have been in at least 3 films?
-// Who has a name that starts with "Bob"?
+console.log(`Who has a name that starts with "Bob"?`);
+for (const member of academyMembers) {
+  if (member.name.indexOf("Bob") != -1) {
+    console.log(member.name);
+  }
+}
+
+//
 // HARDER: Which Academy Members have been in a film
 // that starts with "A"
+
+console.log(`Which Academy Members have been in a film that starts with "A"`);
+for (const member of academyMembers) {
+  for (const filmName of member.films) {
+    if (filmName.indexOf("A") == 0) {
+      console.log(filmName);
+    }
+  }
+}
